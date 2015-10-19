3682851d2b1db184cc8525e2cd5abe190dd4236c
Lợi Nguyễn <loinguyen@chotot.vn>
2015-10-19 14:20:13 +0700

    add array shuffle

--- java/Main.java
+++ java/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
 	public static void main(String args []) {
 
-		EightQueen queen = new EightQueen();
-		queen.solve(0);
+		// EightQueen queen = new EightQueen();
+		// queen.solve(0);
+		int [] array = {2,4,5,6,7,8,9};
+		array =  ArrayShuffle.shuffle(array);
+		Utils.printArray(array);
 	}
 }
