d734883521f69b49cecc540ce60c7f372d9ee2e3
Lợi Nguyễn <loinguyen@chotot.vn>
2015-10-21 21:52:32 +0700

    add permutation

--- java/Main.java
+++ java/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
 		// EightQueen queen = new EightQueen();
 		// queen.solve(0);
-		// int [] array = {2,4,5,6,7,8,9};
+		  int [] array = {1,2,3};
+		//int [] array  = {1,2,3,4,5,6,7};
+
 		// array =  ArrayShuffle.shuffle(array);
 		// Utils.printArray(array);
-		System.out.println(Prime.isPrime(12)+"");
+		//System.out.println(Prime.isPrime(12)+"");
+		Permutation perm = new Permutation(array);
+		perm.printPermutate();
 	}
 }
